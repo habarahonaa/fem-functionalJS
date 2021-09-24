5b354a92f62bb979cbc6f3f476db304735c9339f
habarahonaa <67131053+habarahonaa@users.noreply.github.com>
2021-09-24 08:27:34 -0600

    Added eachRight() and reduce()

--- list-tranforms.js
+++ list-tranforms.js
@@ -32,3 +32,33 @@ _.from = arr => {
         return newArr;
     */
 }
+
+_.forEachRight = function(collection, cb) {
+    // Loops through collection from right to left calling the passed function as cb
+    let colLength = collection.length - 1;
+    for(let i = colLength; i >= 0; i--) {
+      cb(collection[i], i, collection);
+    }
+  };
+  
+  /* console.log([1,2].length-1) */
+  const arr = [1,2,3];
+  
+  _.forEachRight(arr, value => console.log(value));;
+  _.reduce = function(collection, callback, accumulator) {
+    let memo;
+    // If no accumulator is passed to reduce, set the accumulator to equal the first element of the collection
+    if (accumulator === undefined) {
+      memo = collection[0];
+      for(let i = 1; i < collection.length; i++) {
+        memo = callback(collection[i], memo);
+      }
+    } else {
+    // Else, set memo to equal the passed accumulator
+      memo = accumulator;
+      for(let i = 0; i < collection.length; i++) {
+        memo = callback(collection[i], memo);
+      }
+    }
+    return memo;
+  };
